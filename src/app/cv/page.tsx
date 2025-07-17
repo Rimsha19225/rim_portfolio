a68855c7adfeea689cd25712ec99259b1b060612
Rimsha Arshad <rimshaarshad374@gmail.com>
2025-07-18 00:23:25 +0500

    r

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/navbar'
 
 const Cv = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-[#ffe4c4] to-black overflow-x-hidden w-full h-full">
+    <div className="min-h-screen bg-[#d5bea2] md:bg-gradient-to-r md:from-[#ffe4c4] md:to-black overflow-x-hidden w-full h-full">
       <Navbar />
       <div className="flex justify-center items-center h-auto">
         <div className="flex w-full gap-4 px-6 justify-between items-center max-w-[100%] md:max-w-[40%]">
