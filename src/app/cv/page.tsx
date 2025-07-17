1914abb2da7b6a757cc611426cfff6b38506080f
Rimsha Arshad <rimshaarshad374@gmail.com>
2025-07-18 00:17:29 +0500

    hi

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -6,7 +6,7 @@ const Cv = () => {
     <div className="min-h-screen bg-gradient-to-r from-[#ffe4c4] to-black overflow-x-hidden w-full h-full">
       <Navbar />
       <div className="flex justify-center items-center h-auto">
-        <div className="flex w-full gap-4 px-6 justify-between items-center max-w-[40%]">
+        <div className="flex w-full gap-4 px-6 justify-between items-center max-w-[100%] md:max-w-[40%]">
             <iframe
                 src="/docs/RimshaResume.pdf"
                 className="w-full h-[60vh] my-16"
--- src/components/mainpage.tsx
+++ src/components/mainpage.tsx
@@ -41,7 +41,7 @@ const MainPageText = () => {
 
 
   return (
-    <div className="min-h-screen bg-[#d5bea2] md:bg-gradient-to-r md:from-[#ffe4c4] md:to-black overflow-x-hidden w-full h-full">
+    <div className="min-h-screen bg-[#d5bea2] md:bg-gradient-to-r md:from-[#ffe4c4] md:to-black overflow-x-hidden w-full h-auto md:h-full">
       <Navbar />
       <div ref={sectionRef} className="flex justify-center items-center h-[45vh] md:h-[72vh]">
         <div className="flex flex-col-reverse md:flex-row w-full gap-4 px-2 md:px-6 justify-between items-center max-w-[60rem]">
